a774029de5c8255218a975de8b388b7d3b762cf0
Zoran Markovic <markonimobile@gmail.com>
2016-01-05 14:33:30 +0100

    zm-superCell remove esm6

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,6 @@
 //Probably we should include all js files here like in style.scss
 ////////////////////////////////////
 
-// Use es6
-// import {es6Welcome} from './demoES6';
-// es6Welcome(6);
-// https://github.com/ModuleLoader/es6-module-loader/wiki/Brief-Overview-of-ES6-Module-syntax
-
-
 /////////// Ovo mi je u stvari DIST gde pozivam sve js fajlove
 
 //Test
